b57506353e801a856c09476cc79781f808f4c8f5
JOYMUNDIA <joymundia.joymundia@gmail.com>
2023-04-29 07:25:54 -0700

    not e nor q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - whether the number stored in the variable n is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
